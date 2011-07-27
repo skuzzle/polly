5e9480b66e63ef20f3314685f902cacd188c22dc
Simon Taddiken <simont@tzi.de>
2011-07-27 10:18:46 +0200

    Auth command signature added. now you can register without having to enter your username if it equals your irc nick

--- polly.core/src/commands/AuthCommand.java
+++ polly.core/src/commands/AuthCommand.java
@@ -11,12 +11,14 @@ import de.skuzzle.polly.sdk.exceptions.UnknownUserException;
 import de.skuzzle.polly.sdk.model.User;
 
 
+
 public class AuthCommand extends Command {
 
     public AuthCommand(MyPolly polly) throws DuplicatedSignatureException {
         super(polly, "auth");
         this.createSignature("Meldet den Benutzer bei Polly an", new UserType(), 
                 new StringType());
+        this.createSignature("Meldet den Benutzer bei Polly an", new StringType());
         this.setHelpText("Befehl um dich bei Polly anzumelden.");
     }
     
@@ -35,7 +37,7 @@ public class AuthCommand extends Command {
             Signature signature) {
         
         this.reply(channel, "Dieser Befehl ist nur im Query ausführbar. " +
-        		"Zudem solltest du dein Passwort Ändern sofern du es hier für jeden " +
+        		"Zudem solltest du dein Passwort ändern sofern du es hier für jeden " +
         		"lesbar angegeben hast.");
     }
     
@@ -43,27 +45,30 @@ public class AuthCommand extends Command {
     
     @Override
     protected void executeOnQuery(User executer, Signature signature) {
+        String userName = "";
+        String password = "";
         if (this.match(signature, 0)) {
-            String userName = signature.getStringValue(0);
-            String password = signature.getStringValue(1);
+            userName = signature.getStringValue(0);
+            password = signature.getStringValue(1);
+        } else if (this.match(signature, 1)) {
+            userName = executer.getCurrentNickName();
+            password = signature.getStringValue(0);
+        }
+        User user = null;
+        try {
+            user = this.getMyPolly().users().logon(executer.getCurrentNickName(), 
+                    userName, password);
             
-            User user = null;
-            try {
-                user = this.getMyPolly().users().logon(executer.getCurrentNickName(), 
-                        userName, password);
-                
-                if (user == null) {
-                    this.reply(executer, "Falsches Passwort.");
-                    return;
-                }
-                this.reply(executer, "Du bist jetzt angemeldet.");
-            } catch (UnknownUserException e) {
-                this.reply(executer, "Unbekannter Benutzername: " + userName);
-            } catch (AlreadySignedOnException e) {
-                this.reply(executer, "Der Benutzer ist bereits angemeldet.");
+            if (user == null) {
+                this.reply(executer, "Falsches Passwort.");
+                return;
             }
+            this.reply(executer, "Du bist jetzt angemeldet.");
+        } catch (UnknownUserException e) {
+            this.reply(executer, "Unbekannter Benutzername: " + userName);
+        } catch (AlreadySignedOnException e) {
+            this.reply(executer, "Der Benutzer ist bereits angemeldet.");
         }
     }
-
 }
     
