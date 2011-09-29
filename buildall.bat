42c84ce7d1f390e96d007968b3a743e7bc648ece
Simon Taddiken <simont@tzi.de>
2011-09-29 21:42:50 +0200

    build update

--- buildall.bat
+++ buildall.bat
@@ -5,7 +5,7 @@ call ant -f polly.sdk/build.xml
 copy polly.sdk\dist\de.skuzzle.polly.sdk.jar polly.core\lib\de.skuzzle.polly.sdk.jar
 copy polly.sdk\dist\de.skuzzle.polly.sdk.jar polly\lib\de.skuzzle.polly.sdk.jar
 
-call ant -f polly.core/TestPlugin.xml
+call ant -f polly.core/build.xml
 copy polly.core\dist\polly.core.jar polly\cfg\plugins\polly.core.jar
 
 call ant -f polly/build.xml
