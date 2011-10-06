7a69bf93c478146cd9f69170cd62c9262d5d46a3
Simon Taddiken <simont@tzi.de>
2011-10-06 14:21:03 +0200

    -better log output for installer -fixed parameter thingy

--- installer/src/de/skuzzle/polly/installer/Installer.java
+++ installer/src/de/skuzzle/polly/installer/Installer.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 import de.skuzzle.polly.installer.util.Environment;
 import de.skuzzle.polly.installer.util.FileUtil;
 import de.skuzzle.polly.installer.util.PollyConfiguration;
@@ -30,6 +36,14 @@ public class Installer {
             log = new TreeStream(System.out, 4);
         }
         log.println("INSTALL LOG FROM: " + (new Date()).toString());
+        
+        log.println("ENVIRONMENT");
+        log.indent();
+        log.println("POLLY_HOME: " + Environment.POLLY_HOME.getAbsolutePath());
+        log.println("POLLY_CONFIG: " + Environment.POLLY_CONFIG_DIR.getAbsolutePath());
+        log.println("POLLY_CFG: " + Environment.POLLY_CONFIG_FILE.getAbsolutePath());
+        log.println("POLLYPLUGINS: " + Environment.POLLY_PLUGIN_DIR.getAbsolutePath());
+        log.unindent();
         
         for (int i = 0; i < args.length; ++i) {
             try {
@@ -66,7 +80,7 @@ public class Installer {
                 throw new IOException("polly configuration error");
             }
         } catch (IOException e) {
-            log.println("environment error: " + e.getMessage());
+            e.printStackTrace(log);
             return;
         }
         
@@ -74,23 +88,32 @@ public class Installer {
         if (fileNames != null && !fileNames.isEmpty()) {
             updateInfo = installAll(fileNames, log);
         } else {
-            System.out.println("No files to install");
+            log.println("NO FILES TO INSTALL");
             return;
         }
         
         if (runPolly) {
-            updateInfo = !updateInfo.equals("") 
-                    ? "-updateinfo \"" + updateInfo + "\" " 
-                    : "";
-            // final copy
-            final String cmd = "java -jar polly.jar -update false " + updateInfo + 
-                    pollyParams;
-            log.println("EXECUTING: " + cmd);
+            final List<String> cmd = new ArrayList<String>(10);
+            cmd.add("java");
+            cmd.add("-jar");
+            cmd.add("polly.jar");
+            cmd.add("-update");
+            cmd.add("false");
+            if (!updateInfo.equals("")) {
+                cmd.add("-updateinfo");
+                cmd.add(updateInfo);
+            }
+            
+            if (!pollyParams.equals("")) {
+                cmd.addAll(Installer.parsePollyCommands(pollyParams));
+            }
+
+            log.println("EXECUTING: " + cmd.toString());
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 @Override
                 public void run() {
                     try {
-                        Runtime.getRuntime().exec(cmd);
+                        Runtime.getRuntime().exec(cmd.toArray(new String[cmd.size()]));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -106,16 +129,21 @@ public class Installer {
         File backup = null;
         try {
             log.println("CREATING BACKUP");
+            log.indent();
             backup = FileUtil.createTempDirectory();
+            log.println("BACKUP DIRECTORY: " + backup.getAbsolutePath());
             FileUtil.copyContent(Environment.POLLY_HOME, backup);
         } catch (IOException e) {
-            log.println("ERROR WHILE CREATING BACKUP: " + e.toString());
+            log.println("ERROR WHILE CREATING BACKUP");
+            e.printStackTrace(log);
             log.println("DELETING DOWNLOADS");
             for (String file : fileNames) {
                 new File(file).delete();
                 new File(file).deleteOnExit();
             }
-            return "";
+            return "Error while updating: " + e.getMessage();
+        } finally {
+            log.unindent();
         }
         
         String updateInfo = "";
@@ -128,7 +156,7 @@ public class Installer {
                 updateInfo = "Error while updating: " + e.getMessage();
                 log.println("ERROR WHILE INSTALLING UPDATE FROM FILE: " + file);
                 log.indent();
-                log.println(e.getMessage());
+                e.printStackTrace(log);
                 log.unindent();
                 log.println("ROLLING BACK ALL CHANGES");
                 try {
@@ -136,7 +164,8 @@ public class Installer {
                     FileUtil.deleteRecursive(backup);
                 } catch (IOException e1) {
                     updateInfo += "; Error while restoring backup: " + e.getMessage();
-                    log.println("ERROR WHILE RESTORING BACKUP: " + e1.getMessage());
+                    log.println("ERROR WHILE RESTORING BACKUP");
+                    e1.printStackTrace(log);
                     return updateInfo;
                 }
                 
@@ -182,5 +211,22 @@ public class Installer {
             log.println(file.getAbsolutePath());
         }
         log.unindent();
+    }
+    
+    
+    
+    private static List<String> parsePollyCommands(String cmd) {
+        Pattern p = Pattern.compile("\\S+|\"[^\"]+\"");
+        Matcher m = p.matcher(cmd);
+        List<String> result = new LinkedList<String>();
+        while (m.find()) {
+            String found = cmd.substring(m.start(), m.end());
+            if (found.startsWith("\"")) {
+                assert found.endsWith("\"");
+                found = found.substring(1, found.length() - 1);
+            }
+            result.add(found);
+        }
+        return result;
     }
 }
