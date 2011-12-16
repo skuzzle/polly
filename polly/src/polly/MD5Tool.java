25090f92e79e0776722269433e2c138e6f8bdf70
Simon Taddiken <simon-taddiken@gmx.de>
2011-12-16 14:54:37 +0100

    little improvement for md5tool: now you can insert the password via std.in if no argument is provided

--- polly/src/polly/MD5Tool.java
+++ polly/src/polly/MD5Tool.java
@@ -11,12 +11,15 @@ import polly.data.User;
 public class MD5Tool {
 
     public static void main(String[] args) throws IOException, ConfigurationFileException {
+        String pw = "";
         if (args.length != 1) {
-            System.out.println("Bitte nur einen Parameter angeben.");
-            return;
+            System.out.println("Bitte gib dein gewünschtes Password ein:");
+            BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+            pw = r.readLine();
+        } else {
+            pw = args[0];
         }
-        
-        String pw = args[0];
+
         User tmp = new User("", pw, 0);        
         System.out.println(tmp.getHashedPassword());
         System.out.println(
