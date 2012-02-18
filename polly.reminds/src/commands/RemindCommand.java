71d2c965c5915985f37146a1139c544003c5a4a6
Thomas Pummer <dev@nullpointer.at>
2012-02-18 21:30:48 +0100

    fast remind with time only

--- polly.reminds/src/commands/RemindCommand.java
+++ polly.reminds/src/commands/RemindCommand.java
@@ -3,6 +3,10 @@ package commands;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import polly.reminds.MyPlugin;
+
 import core.RemindManager;
 import de.skuzzle.polly.sdk.MyPolly;
 import de.skuzzle.polly.sdk.Signature;
@@ -12,15 +16,19 @@ import de.skuzzle.polly.sdk.Types.ListType;
 import de.skuzzle.polly.sdk.Types.StringType;
 import de.skuzzle.polly.sdk.Types.UserType;
 import de.skuzzle.polly.sdk.exceptions.CommandException;
+import de.skuzzle.polly.sdk.exceptions.DatabaseException;
 import de.skuzzle.polly.sdk.exceptions.DuplicatedSignatureException;
+import de.skuzzle.polly.sdk.exceptions.UnknownAttributeException;
 import de.skuzzle.polly.sdk.model.User;
 import entities.RemindEntity;
 
 
 
 public class RemindCommand extends AbstractRemindCommand {
+	
+	protected Logger logger = Logger.getLogger(RemindCommand.class.getName());
 
-    public RemindCommand(MyPolly myPolly, RemindManager remindManager) 
+	public RemindCommand(MyPolly myPolly, RemindManager remindManager) 
             throws DuplicatedSignatureException {
         super(myPolly, remindManager, "remind");
         this.createSignature("Erinnert den Benutzer zu einer angegebenen Zeit im " +
@@ -34,9 +42,13 @@ public class RemindCommand extends AbstractRemindCommand {
                 new DateType(), new StringType());
         this.createSignature("Erinnert den angegebenen Benutzer per Query an etwas.", 
             new UserType(), new DateType(), new StringType());
+        this.createSignature("Erinnert dich zu einer bestimmten Zeit mit einer Standardmeldung.",
+        		new DateType());
         this.setRegisteredOnly();
         this.setHelpText("Hinterlässt Erinnerungen für Benutzer.");
     }
+	
+	
     
     
     
@@ -93,6 +105,19 @@ public class RemindCommand extends AbstractRemindCommand {
 
             RemindEntity remind = new RemindEntity(msg, fromUser, forUser, forUser, 
                     dueDate);
+            this.addRemind(remind, true);
+            this.reply(channel, FORMATTER.format(remind, this.getMyPolly().formatting()));
+        } else  if (this.match(signature, 4)){
+        	Date dueDate = signature.getDateValue(0);
+        	String msg = "Reminder";
+        	try {
+                msg = executer.getAttribute(MyPlugin.DEFAULT_MSG);
+            } catch (UnknownAttributeException e) {
+                throw new CommandException(e);
+            }
+            
+            RemindEntity remind = new RemindEntity(msg, executer.getCurrentNickName(), 
+                    executer.getCurrentNickName(), channel, dueDate);
             this.addRemind(remind, true);
             this.reply(channel, FORMATTER.format(remind, this.getMyPolly().formatting()));
         }
