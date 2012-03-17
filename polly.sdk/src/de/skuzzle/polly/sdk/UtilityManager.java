450c61c3432937d0f845c3ae87576995e1f49916
Simon Taddiken <simon-taddiken@gmx.de>
2012-03-17 12:19:39 +0100

    UtilityManager interface

---
+++ polly.sdk/src/de/skuzzle/polly/sdk/UtilityManager.java
@@ -0,0 +1,12 @@
+package de.skuzzle.polly.sdk;
+
+
+/**
+ * This class provides several utility methods that can be used by any plugin.
+ * 
+ * @author Simon
+ * @since 0.9
+ */
+public interface UtilityManager {
+
+}
