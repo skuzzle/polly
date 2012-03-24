ec3de5291ab1f1543da262dee6d39ade9741c3c1
Simon Taddiken <simon-taddiken@gmx.de>
2012-03-24 13:29:40 +0100

    fixed exception to conform to java 6

--- polly.sdk/src/de/skuzzle/polly/sdk/exceptions/ConstraintException.java
+++ polly.sdk/src/de/skuzzle/polly/sdk/exceptions/ConstraintException.java
@@ -9,11 +9,6 @@ public class ConstraintException extends Exception {
         super();
     }
 
-    public ConstraintException(String arg0, Throwable arg1, boolean arg2,
-        boolean arg3) {
-        super(arg0, arg1, arg2, arg3);
-    }
-
     public ConstraintException(String arg0, Throwable arg1) {
         super(arg0, arg1);
     }
@@ -25,7 +20,4 @@ public class ConstraintException extends Exception {
     public ConstraintException(Throwable arg0) {
         super(arg0);
     }
-
-    
-    
 }
