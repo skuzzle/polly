6aa83e1ab8fdbb741a867c7b09a8d9ececa1118d
Simon Taddiken <simon-taddiken@gmx.de>
2012-04-13 16:14:04 +0200

    added 2 missing classes

---
+++ polly.core/src/commands/ExportAttributesCommand.java
@@ -0,0 +1,66 @@
+package commands;
+
+
+import de.skuzzle.polly.sdk.Command;
+import de.skuzzle.polly.sdk.MyPolly;
+import de.skuzzle.polly.sdk.Parameter;
+import de.skuzzle.polly.sdk.Signature;
+import de.skuzzle.polly.sdk.Types;
+import de.skuzzle.polly.sdk.exceptions.CommandException;
+import de.skuzzle.polly.sdk.exceptions.DuplicatedSignatureException;
+import de.skuzzle.polly.sdk.exceptions.InsufficientRightsException;
+import de.skuzzle.polly.sdk.model.User;
+
+
+public class ExportAttributesCommand extends Command {
+
+    public ExportAttributesCommand(MyPolly polly) 
+                throws DuplicatedSignatureException {
+        super(polly, "expattr");
+        this.createSignature("Exportiert alle deine polly Attribute");
+        this.createSignature("Exportiert alle polly Attribute des angegebenen Benutzers", 
+            new Parameter("Benutzer", Types.USER));
+        this.setHelpText("Mit diesem Befehl können die individuellen polly Attributes " +
+        		"eines Benutzers exportiert werden.");
+        this.setRegisteredOnly();
+    }
+    
+    
+    
+    @Override
+    protected boolean executeOnBoth(User executer, String channel, Signature signature) 
+                throws CommandException, InsufficientRightsException {
+        
+        User user = executer;
+        if (this.match(signature, 1)) {
+            user = this.getMyPolly().users().getUser(signature.getStringValue(0));
+            if (user == null) {
+                throw new CommandException("Benutzer '" + signature.getStringValue(0) + 
+                    "' existiert nicht");
+            }
+        }
+        this.reply(channel, this.export(user));
+        
+        return false;
+    }
+    
+    
+    
+    private String export(User user) throws CommandException {
+        StringBuilder b = new StringBuilder();
+        for (String att : user.getAttributeNames()) {
+            b.append(":setattr ");
+            b.append(att);
+            b.append(" \"");
+            b.append(user.getAttribute(att));
+            b.append("\"\n");
+        }
+        
+        try {
+            return this.getMyPolly().pasting().getRandomService().paste(b.toString());
+        } catch (Exception e) {
+            throw new CommandException(e);
+        }
+    }
+
+}
---
+++ polly/src/polly/util/Hashes.java
@@ -0,0 +1,21 @@
+package polly.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class Hashes {
+
+    public static String md5(String s) {
+        String result = "";
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(s.getBytes(), 0, s.length());
+            result = new BigInteger(1, m.digest()).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+}
