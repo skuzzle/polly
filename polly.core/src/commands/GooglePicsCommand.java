498262829550bd31ad2056ddd45345d62891299c
Simon Taddiken <simon-taddiken@gmx.de>
2012-05-05 21:01:15 +0200

    completed google picture command

--- polly.core/src/commands/GooglePicsCommand.java
+++ polly.core/src/commands/GooglePicsCommand.java
@@ -1,13 +1,19 @@
 package commands;
 
 import de.skuzzle.polly.sdk.MyPolly;
+import de.skuzzle.polly.sdk.Parameter;
+import de.skuzzle.polly.sdk.Types;
+import de.skuzzle.polly.sdk.exceptions.DuplicatedSignatureException;
 
 
 public class GooglePicsCommand extends SearchEngineCommand {
 
-    public GooglePicsCommand(MyPolly myPolly) {
+    public GooglePicsCommand(MyPolly myPolly) throws DuplicatedSignatureException {
         super(myPolly, "pix");
+        this.createSignature("Gibt einen Link zur google Bilder Suche zurück", 
+            new Parameter("Suchbegriff", Types.STRING));
     }
+    
 
     
     @Override
