1db7e73faa0e5f768d180e2bbddfdda1d3cfde22
Simon Taddiken <simon-taddiken@gmx.de>
2012-07-27 07:25:26 +0200

    added missing file

---
+++ polly/src/polly/core/http/HttpInterface.java
@@ -0,0 +1,20 @@
+package polly.core.http;
+
+
+
+public interface HttpInterface {
+    
+    public final static String CONTENT = "content";
+    
+    public final static String PAGE_ERROR = "pages/error.html";
+    public final static String PAGE_IRC = "pages/irc.html";
+    public final static String PAGE_LOGIN = "pages/login.html";
+    public final static String PAGE_HOME = "pages/home.html";
+    public static final String PAGE_USER_INFO = "pages/user_info.html";
+    public static final String PAGE_USERS = "pages/users.html";
+    
+    
+    public final static String ERROR_HEADING = "errorHeading";
+    public final static String ERROR_DESCRIPTION = "errorDescription";
+
+}
