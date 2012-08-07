d8d28e57626af7224bdfa2d0bd6658b5774bbea0
Simon Taddiken <simon.taddiken@gmail.com>
2012-08-07 22:18:01 +0200

    allow web login for all registered users

--- polly/src/polly/core/http/actions/LoginHttpAction.java
+++ polly/src/polly/core/http/actions/LoginHttpAction.java
@@ -5,23 +5,20 @@ import de.skuzzle.polly.sdk.MyPolly;
 import de.skuzzle.polly.sdk.UserManager;
 import de.skuzzle.polly.sdk.http.HttpAction;
 import de.skuzzle.polly.sdk.http.HttpEvent;
-import de.skuzzle.polly.sdk.http.HttpManager;
 import de.skuzzle.polly.sdk.http.HttpTemplateContext;
 import de.skuzzle.polly.sdk.model.User;
-import de.skuzzle.polly.sdk.roles.RoleManager;
+
 
 
 public class LoginHttpAction extends HttpAction {
 
     private UserManager userManager;
-    private RoleManager roleManager;
     
     
     
     public LoginHttpAction(MyPolly myPolly) {
         super("/login", myPolly);
         this.userManager = myPolly.users();
-        this.roleManager = myPolly.roles();
     }
     
     
@@ -37,9 +34,7 @@ public class LoginHttpAction extends HttpAction {
             context.put("noAction", Boolean.FALSE);
             User u = this.userManager.getUser(userName);
 
-            if (u != null && this.roleManager.hasPermission(
-                        u, HttpManager.HTTP_ADMIN_PERMISSION)) {
-                
+            if (u != null) {
                 if (u.checkPassword(password)) {
                     e.getSession().setUser(u);
                     context.put("success", Boolean.TRUE);
@@ -47,10 +42,7 @@ public class LoginHttpAction extends HttpAction {
                     return e.getSource().errorTemplate("Login Error", 
                         "Invalid login data", e.getSession());
                 }
-            } else if (u != null) {
-                return e.getSource().errorTemplate("Login Error", 
-                    "Insufficient permissions", e.getSession());
-            } else if (u == null) {
+            } else {
                 return e.getSource().errorTemplate("Login Error", 
                     "Invalid login data", e.getSession());
             }
