7924e14488440dd55ef54807c5641e420438ccd6
Simon Taddiken <simon.taddiken@gmail.com>
2012-09-12 17:52:48 +0200

    forgot dat class

---
+++ polly/src/polly/util/InputStreamCounter.java
@@ -0,0 +1,31 @@
+package polly.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class InputStreamCounter extends InputStream {
+
+    private int bytes;
+    private InputStream backend;
+    
+    
+    public InputStreamCounter(InputStream backend) {
+        this.backend = backend;
+    }
+    
+    
+    
+    public int getBytes() {
+        return this.bytes;
+    }
+    
+    
+    
+    @Override
+    public int read() throws IOException {
+        ++this.bytes;
+        return this.backend.read();
+    }
+
+}
