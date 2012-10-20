7f4ea6e979349c73f304de99f0ee211fef1ac4e2
Simon Taddiken <simon.taddiken@gmail.com>
2012-10-20 15:19:25 +0200

    improved reply handler

--- polly/src/polly/eventhandler/HardcodedEggiReplyHandler.java
+++ polly/src/polly/eventhandler/HardcodedEggiReplyHandler.java
@@ -3,26 +3,33 @@ package polly.eventhandler;
 import de.skuzzle.polly.sdk.eventlistener.MessageEvent;
 import de.skuzzle.polly.sdk.eventlistener.MessageListener;
 
-
 public class HardcodedEggiReplyHandler implements MessageListener {
 
     @Override
     public void publicMessage(MessageEvent e) {
-        if (e.getChannel().equals("#regenbogen") && 
-            e.getUser().getNickName().equals("Eggi") && 
-            e.getMessage().toLowerCase().contains("hallo") && 
-            e.getMessage().toLowerCase().contains("polly")) {
+        if (e.getMessage().toLowerCase().contains("hallo")
+            && e.getMessage().toLowerCase().contains(e.getSource().getNickname())) {
             
-            e.getSource().sendMessage("#regenbogen", "Hallo Eggi", this);
+            e.getSource().sendMessage(e.getChannel(),
+                "Hallo " + e.getUser().getNickName(), this);
         }
     }
 
-    @Override
-    public void privateMessage(MessageEvent ignore) {}
+
 
     @Override
-    public void actionMessage(MessageEvent ignore) {}
+    public void privateMessage(MessageEvent ignore) {
+    }
+
+
 
     @Override
-    public void noticeMessage(MessageEvent ignore) {}
+    public void actionMessage(MessageEvent ignore) {
+    }
+
+
+
+    @Override
+    public void noticeMessage(MessageEvent ignore) {
+    }
 }
