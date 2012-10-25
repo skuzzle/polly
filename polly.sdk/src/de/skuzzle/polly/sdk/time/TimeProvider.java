1b2ae0951d42ad0ae9b59540fe677b747678fba0
Simon Taddiken <simon.taddiken@gmail.com>
2012-10-25 11:07:25 +0200

    documentation fix

--- polly.sdk/src/de/skuzzle/polly/sdk/time/TimeProvider.java
+++ polly.sdk/src/de/skuzzle/polly/sdk/time/TimeProvider.java
@@ -1,6 +1,5 @@
 package de.skuzzle.polly.sdk.time;
 
-import de.skuzzle.polly.sdk.MyPolly;
 
 
 
@@ -9,13 +8,13 @@ import de.skuzzle.polly.sdk.MyPolly;
  * and plugins.</p>
  * 
  * <p>Using this class, you can modify the system time in order to debug certain 
- * components. You can use {@link MyPolly#setTimeProvider(TimeProvider)} to install
+ * components. You can use {@link Time#setProvider(TimeProvider)} to install
  * your time provider instance. Note that this method will have no effect if polly is
  * not running in debug mode.</p> 
  * 
  * <p>Note that this class will not change the system time of the machine that polly is
  * running on. Instead it modifies the return value of 
- * {@link MyPolly#currentTimeMillis()}, so all components which use this method to get
+ * {@link Time#currentTimeMillis()}, so all components which use this method to get
  * the current time will use the modified time.</p>
  * 
  * @author Simon
@@ -25,7 +24,7 @@ public interface TimeProvider {
 
     /**
      * This method should return a modified system time in milliseconds. All polly 
-     * components which use {@link MyPolly#pollySystemTime()} to retrieve the current
+     * components which use {@link Time#currentTime()} to retrieve the current
      * time will get the time computed by this method. So it can be used to debug
      * time dependent behavior.
      * 
--- polly/src/polly/core/irc/IrcManagerImpl.java
+++ polly/src/polly/core/irc/IrcManagerImpl.java
@@ -338,7 +338,8 @@ public class IrcManagerImpl extends AbstractDisposable implements IrcManager, Di
         this.bot.sendMessage("nickserv", "ghost " + e.getNickName() + " " + e.getIdentity());
         this.setAndIdentifyDefaultNickname();
         if (this.config.readBoolean(Configuration.JOIN_ON_CONNECT)) {
-            this.joinChannels(e.getChannels().toArray(new String[e.getChannels().size()]));
+            this.joinChannels(
+                e.getChannels().toArray(new String[e.getChannels().size()]));
         }
         this.recent = e;
         this.sendRawCommand("MODE " + e.getNickName() + " " + e.getModes());
