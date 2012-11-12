460506145c7f35ac6458b4dcd402565560fd23ef
Simon Taddiken <simon.taddiken@gmail.com>
2012-11-12 09:07:13 +0100

    added scoreboard manager

---
+++ polly.rx/src/polly/rx/core/ScoreBoardManager.java
@@ -0,0 +1,61 @@
+package polly.rx.core;
+
+import java.util.Collection;
+
+import de.skuzzle.polly.sdk.PersistenceManager;
+import de.skuzzle.polly.sdk.WriteAction;
+import de.skuzzle.polly.sdk.exceptions.DatabaseException;
+import polly.rx.entities.ScoreBoardEntry;
+
+
+public class ScoreBoardManager {
+
+    private PersistenceManager persistence;
+    
+    
+    
+    public ScoreBoardManager(PersistenceManager persistence) {
+        this.persistence = persistence;
+    }
+    
+    
+    
+    public void addEntry(final ScoreBoardEntry entry) throws DatabaseException {
+        this.persistence.atomicWriteOperation(new WriteAction() {
+            
+            @Override
+            public void performUpdate(PersistenceManager persistence) {
+                persistence.persist(entry);
+            }
+        });
+    }
+    
+    
+    
+    public void deleteEntry(int id) throws DatabaseException {
+        ScoreBoardEntry sbe = this.persistence.atomicRetrieveSingle(
+            ScoreBoardEntry.class, id);
+        
+        if (sbe == null) { 
+            return;
+        }
+        
+        this.persistence.atomicRemove(sbe);
+    }
+    
+    
+    
+    
+    public Collection<ScoreBoardEntry> getEntries(String venad) {
+        return this.persistence.atomicRetrieveList(ScoreBoardEntry.class, 
+            ScoreBoardEntry.SBE_BY_USER, venad);
+    }
+    
+    
+    
+    public Collection<ScoreBoardEntry> getEntries() {
+        return this.persistence.atomicRetrieveList(ScoreBoardEntry.class, 
+            ScoreBoardEntry.ALL_SBE_DISTINCT);
+    }
+    
+}
