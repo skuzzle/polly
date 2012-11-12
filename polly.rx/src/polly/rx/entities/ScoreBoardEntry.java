58080fabf3796ed4a404d7f74fa5e5143d3b5500
Simon Taddiken <simon.taddiken@gmail.com>
2012-11-12 09:01:52 +0100

    added queries to ScoreBoardEntry

--- polly.rx/src/polly/rx/entities/ScoreBoardEntry.java
+++ polly.rx/src/polly/rx/entities/ScoreBoardEntry.java
@@ -1,12 +1,31 @@
 package polly.rx.entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import de.skuzzle.polly.sdk.time.Time;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "ALL_SBE_DISTINCT",
+        query= "SELECT DISTINCT(sbe.name) FROM ScoreBoardEntry sbe ORDER BY sbe.date DESC"
+    ),
+    @NamedQuery(
+        name = "SBE_BY_USER",
+        query = "SELECT sbe FROM ScoreBoardEntry sbe WHERE sbe.name = ?1"
+    )
+})
 public class ScoreBoardEntry {
+    
+    public final static String ALL_SBE_DISTINCT = "ALL_SBE_DESTINCT";
+    public final static String SBE_BY_USER = "SBE_BY_USER";
     
     @Id@GeneratedValue(strategy = GenerationType.TABLE)
     private int id;
@@ -17,11 +36,15 @@ public class ScoreBoardEntry {
     
     private int points;
 
+    private Date date;
+    
+    
     public ScoreBoardEntry(String venadName, int rank, int points) {
         super();
         this.venadName = venadName;
         this.rank = rank;
         this.points = points;
+        this.date = Time.currentTime();
     }
 
     
@@ -46,4 +69,7 @@ public class ScoreBoardEntry {
     
     
     
+    public Date getDate() {
+        return this.date;
+    }
 }
