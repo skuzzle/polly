0f9627f6e65a851334030b0857c3030ec4a3cc53
Simon Taddiken <simon.taddiken@gmail.com>
2013-02-25 22:17:15 +0100

    utility: added mulit comparator

--- polly.tools/src/de/skuzzle/polly/tools/CompareUtils.java
+++ polly.tools/src/de/skuzzle/polly/tools/CompareUtils.java
@@ -1,9 +1,80 @@
 package de.skuzzle.polly.tools;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 
 public final class CompareUtils {
+    
+    /**
+     * Comparator implementation that uses several internal comparators, that are 
+     * executed as long as the next comparator returns 0.
+     * If all comparators return 0, 0 will be returned. Otherwise, the first result != 0
+     * will be returned.
+     * 
+     * @author Simon Taddiken
+     * @param <T> Type to compare.
+     */
+    private final static class MultiComparator<T> implements Comparator<T> {
+
+        private final List<Comparator<T>> comps;
+        
+        /**
+         * Creates a new MultiComparator which uses the given list of comparators.
+         * 
+         * @param comps The comparators to execute in the order they occur in the list.
+         */
+        public MultiComparator(List<Comparator<T>> comps) {
+            this.comps = comps;
+        }
+        
+        
+        
+        @Override
+        public int compare(T o1, T o2) {
+            for (final Comparator<T> c : this.comps) {
+                int r = c.compare(o1, o2);
+                if (r != 0) {
+                    return r;
+                }
+            }
+            return 0;
+        }
+    }
+    
+    
+    
+    /**
+     * Creates a comparator that internally uses the list of given comparators to
+     * compare to objects. The returned comparator will return 0 if, and only if all
+     * comparators in the given list return 0. Otherwise, it will return the first
+     * result other than zero that any of the comparators in the list returned. 
+     * Comparators are called in order they occur in the list.
+     * 
+     * @param comps The list of comparators to use.
+     * @return A comparator instance.
+     */
+    public final static <T> Comparator<T> multiComparator(List<Comparator<T>> comps) {
+        return new MultiComparator<T>(comps);
+    }
+    
+
+    
+    /**
+     * Creates a comparator that internally uses the array of given comparators to
+     * compare to objects. The returned comparator will return 0 if, and only if all
+     * comparators in the given array return 0. Otherwise, it will return the first
+     * result other than zero that any of the comparators in the array returned. 
+     * Comparators are called in order they occur in the array.
+     * 
+     * @param comps The list of comparators to use.
+     * @return A comparator instance.
+     */
+    public final static <T> Comparator<T> multiComparator(Comparator<T>... comps) {
+        return new MultiComparator<T>(Arrays.asList(comps));
+    }
+    
     
 
     /**
