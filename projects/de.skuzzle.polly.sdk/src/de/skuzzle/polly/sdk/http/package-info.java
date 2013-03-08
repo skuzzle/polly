10f646993c069645a7e84e5f6d764b5ee8ec40a5
Simon Taddiken <simon.taddiken@gmail.com>
2013-03-08 16:42:16 +0100

    package infos

---
+++ projects/de.skuzzle.polly.sdk/src/de/skuzzle/polly/sdk/http/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package which contains API to access polly's web interface.
+ * 
+ * @author Simon
+ */
+package de.skuzzle.polly.sdk.http;
---
+++ projects/de.skuzzle.polly.sdk/src/de/skuzzle/polly/sdk/roles/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package which contains API to access polly's role management.
+ * 
+ * @author Simon
+ */
+package de.skuzzle.polly.sdk.roles;
