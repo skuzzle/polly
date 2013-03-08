b9660c62df4a69458e585cfe92e217b6baaf6ccd
Simon Taddiken <simon.taddiken@gmail.com>
2013-03-08 15:42:51 +0100

    javadoc fix

--- projects/de.skuzzle.polly.sdk/src/de/skuzzle/polly/sdk/constraints/Constraints.java
+++ projects/de.skuzzle.polly.sdk/src/de/skuzzle/polly/sdk/constraints/Constraints.java
@@ -17,7 +17,7 @@ public interface Constraints {
     
     /**
      * Constraints a value to a double.
-     * @see {@link DoubleConstraint}
+     * @see DoubleConstraint
      */
     public final static AttributeConstraint DOUBLE = new DoubleConstraint();
     
