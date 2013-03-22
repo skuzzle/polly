a73ea8cbb791d02cfbadf3bccd0349c710f03a60
Simon Taddiken <simon.taddiken@gmail.com>
2013-03-22 16:51:16 +0100

    api documentation

--- projects/de.skuzzle.polly.http/src/de/skuzzle/polly/http/api/HttpSession.java
+++ projects/de.skuzzle.polly.http/src/de/skuzzle/polly/http/api/HttpSession.java
@@ -25,10 +25,20 @@ public interface HttpSession {
     
     public final static int SESSION_TYPE_COOKIE = 1;
     
+    /**
+     * Gets a timestamp of when this session was created.
+     * 
+     * @return Time when this session was created.
+     */
     public long getTimestamp();
     
     public int getType();
     
+    /**
+     * Gets the unique id of this session.
+     * 
+     * @return The unique session id.
+     */
     public String getId();
 
     public void attach(String key, Object item);
@@ -39,13 +49,48 @@ public interface HttpSession {
     
     public void getAttached(String key);
     
+    /**
+     * Gets information on how much bytes has been sent and received over this session.
+     * 
+     * @return A TrafficInformation object.
+     */
     public TrafficInformation getTrafficInfo();
+    
     
     public void kill();
     
+    /**
+     * <p>Blocks this session for the given amount of milliseconds. 
+     * {@link HttpEvent HttpEvents} incoming over a blocked session will not be handled
+     * by the {@link HttpServer HttpServers} event handlers. Instead a simple message
+     * will be sent back saying that the connection is currently blocked.</p>
+     * 
+     * <p>Blocking and unblocking sessions is thread-safe.</p>
+     * 
+     * @param milliseconds Time of how long the session should be blocked. Specify a 
+     *          negative value if you want to block until its manually unblocked.
+     * @see #unblock()
+     * @see #isBlocked()
+     */
     public void block(int milliseconds);
     
+    /**
+     * Unblocks the session right now. If it is not blocked, calling this method will
+     * have no effect.
+     * 
+     * <p>Blocking and unblocking sessions is thread-safe.</p>
+     * 
+     * @see #block(int)
+     * @see #isBlocked()
+     */
     public void unblock();
     
+    /**
+     * Determines whether this session is currently blocked.
+     * 
+     * @return Whether the session is currently blocked.
+     * @see #block(int)
+     * @see #unblock()
+     */
     public boolean isBlocked();
 }
