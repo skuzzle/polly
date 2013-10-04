f69b8f0e1a16a58b69a87c7dad25c65785ee8626
Simon Taddiken <simon.taddiken@gmail.com>
2013-10-04 16:51:04 +0200

    Added scripts without passwords

---
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/httpv2/view/kbreport.user.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name        revorix.de KB Report
+// @namespace   qzone.servebbs.net
+// @author      smash,betty, qzone
+// @description Script um die Revorix-Kampfberichte an den Polly-Bot zu schicken
+// @include     http://www.revorix.info/php/news_pop.php*
+// @include		http://www.revorix.info/php/map_attack.php?fida=*
+// @grant       GM_xmlhttpRequest
+// @grant       GM_log
+// @version     0.3
+// ==/UserScript==
+
+var verbose = true; // prints status message in table header
+var consolelog = false; // log messages in console
+
+var pollyUserName = "rxScoreBoardPaster";
+var pollyPassword = "TODO";
+var pollyUrl = "$host";
+var pollyApi = "$api";
+var isLiveKb = false;
+
+function log(message) {
+	if(consolelog) {
+		GM_log(message);
+	}
+}
+
+function getData() {
+	var tableNode = null;
+	if(isLiveKb) {
+    	tableNode = getElementByXPath("/html/body/div[2]/div[2]/div/table/tbody");
+    } else {
+    	tableNode = getElementByXPath("/html/body/table/tbody");
+    }
+    var postData = tableNode.textContent;
+    return postData;
+}
+
+function sendData(postData) {
+    log(postData);
+    printStatus("Sending data...");
+    GM_xmlhttpRequest({
+        timeout: 5000,
+        method: "POST",
+        url: pollyUrl + pollyApi,
+        data: "user="+pollyUserName + "&pw="+pollyPassword + "&report=" + encodeURI(postData),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+        	//It seems like this Battlereport already exists
+            var result = JSON.parse(response.responseText);
+            printStatus(result.message);
+        },
+        ontimeout: function(response) {
+            printStatus("Timeout while sending data!");
+            log("Timeout while sending data!");
+        }
+    }); 
+}
+
+function getElementByXPath(path) {
+  result = document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  return result.singleNodeValue;
+}
+
+function isKB() {
+	// kb of stored message
+    var node = getElementByXPath("/html/body/table/tbody/tr[2]/td/table/tbody/tr/td");
+    try {
+	    if(node.innerHTML.indexOf("Zurückgelassene Ressourcen") != -1) {
+    		return true;
+    	}
+    } catch(e) {
+		// live kb
+    	node = getElementByXPath("/html/body/div[2]/div[2]/div/table/tbody/tr/td/table/tbody/tr/td");
+    	try {
+    		if(node.innerHTML.indexOf("Zurückgelassene Ressourcen") != -1) {
+    			isLiveKb = true;
+    			return true;
+    		}
+    	} catch(e) {
+    		return false;
+    	}
+    }
+    return false;
+}
+    	
+
+function printStatus(status) {
+    if (!verbose) {
+        return;
+    }
+    if(status != "") {
+    	status = " - " + status;
+    }
+    var node = getElementByXPath("/html/body/table/tbody/tr[2]/td/table/tbody/tr/td");
+    try {
+	    if(node.innerHTML.indexOf("Zurückgelassene Ressourcen") != -1) {
+    		node.innerHTML = "Zurückgelassene Ressourcen" + status;
+    	}
+    } catch(e) {
+		// live kb
+    	node = getElementByXPath("/html/body/div[2]/div[2]/div/table/tbody/tr/td/table/tbody/tr/td");
+    	try {
+    		if(node.innerHTML.indexOf("Zurückgelassene Ressourcen") != -1) {
+    			node.innerHTML = "Zurückgelassene Ressourcen" + status;
+    		}
+    	} catch(e) {
+    		;
+    	}
+    }
+}
+
+// run the script
+if(isKB()) {
+	sendData(getData());
+}
---
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/httpv2/view/scrapescoreboarddata.user.js
@@ -0,0 +1,128 @@
+// ==UserScript==
+// @name        revorix.de UmgebungslistenScript
+// @namespace   skuzzle.de
+// @author      smash,betty
+// @description Script um die Revorix-Umgebungsliste an den Polly-Bot zu schicken
+// @include     http://www.revorix.info/php/venad_list.php?pktsur=1
+// @include     http://www.revorix.info/php/venad_list.php?pkttop=1
+// @version     0.9
+// ==/UserScript==
+
+var pollyUserName = "rxScoreBoardPaster"
+var pollyPassword = "TODO"
+var pollyUrl = "$host"
+var verbose = true; // prints status message in table header
+
+
+// run the script
+
+GM_log("running....");
+if (document.baseURI.indexOf("pktsur=1") != -1) {
+    GM_log("chosing environment");
+    sendData(getData());
+} else {
+    GM_log("chosing top50");
+    sendData(getTop50Data());
+}
+
+
+
+
+function getData() {
+
+    var tableData = document.getElementsByTagName("td");
+    var postData = "";
+
+    var start = 4; // skip header
+    var length = tableData.length - 1; // skip footer
+    
+    for (var i = start; i <= length; i++) {       
+        if (tableData[i].firstChild.tagName == 'A') { //venad with profile and clantag
+            postData += tableData[i].firstChild.textContent;
+            if (tableData[i].firstChild.nextSibling) { //venad with profile without clantag
+                postData += tableData[i].firstChild.nextSibling.textContent;
+            }
+        } else { //venad without profile and without clantag
+            postData += tableData[i].firstChild.textContent;
+        }
+
+        if (i%3 == 0) {
+            postData += "\n";
+        } else {
+            postData += " ";
+        }
+    }
+    return postData;
+}
+
+
+
+function getTop50Data() {
+    var tableData = document.getElementsByTagName("td");
+    var postData = "";
+    
+    var start = 5; // skip header
+    var length = tableData.length - 1; // skip footer
+    
+    var col = 0;
+    for (var i = start; i <= length; i++) {
+        if (col == 2) {
+            col++;
+            continue; // skip "Titel" column;
+        }
+        
+        if (tableData[i].firstChild.tagName == 'A') { //venad with profile and clantag
+            postData += tableData[i].firstChild.textContent;
+            if (tableData[i].firstChild.nextSibling) { //venad with profile without clantag
+                postData += tableData[i].firstChild.nextSibling.textContent;
+            }
+        } else { //venad without profile and without clantag
+            postData += tableData[i].firstChild.textContent;
+        }
+        
+        if (col++ == 3) {
+            postData += "\n";
+            col = 0;
+        } else {
+            postData += " ";
+        }
+    }
+    return postData;
+}
+
+
+
+function sendData(postData) {
+    GM_log(postData);
+    printStatus("Sending data...");
+    GM_xmlhttpRequest({
+        timeout: 25000,
+        method: "POST",
+        url: pollyUrl + "/postScoreboard",
+        data: "user="+pollyUserName + "&pw="+pollyPassword + "&paste=" + encodeURI(postData),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+            var result = JSON.parse(response.responseText);
+            printStatus(result.message);
+        },
+        ontimeout: function(response) {
+            printStatus("Timeout while sending data!");
+            if (response.responseText.indexOf("Success!") == -1) {
+                printStatus("Error while sending data");
+            }
+        }
+    });    
+}
+
+
+
+function printStatus(status) {
+    if (!verbose) {
+        return;
+    }
+    var elements = document.getElementsByTagName("td");
+    var first = elements[0];
+    first.innerHTML = status;
+}
