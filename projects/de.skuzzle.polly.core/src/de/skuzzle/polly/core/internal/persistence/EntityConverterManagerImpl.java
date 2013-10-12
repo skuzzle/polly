89a3bbee7c9643b9cfed832e2f21399303e8b5d8
Simon Taddiken <simon.taddiken@gmail.com>
2013-10-12 21:47:32 +0200

    Single transaction for each entity converter

--- projects/de.skuzzle.polly.core/src/de/skuzzle/polly/core/internal/persistence/EntityConverterManagerImpl.java
+++ projects/de.skuzzle.polly.core/src/de/skuzzle/polly/core/internal/persistence/EntityConverterManagerImpl.java
@@ -12,31 +12,28 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-
 public class EntityConverterManagerImpl {
 
     private final static Logger logger = Logger
-        .getLogger(EntityConverterManagerImpl.class.getName());
-    
-    
+            .getLogger(EntityConverterManagerImpl.class.getName());
+
     private List<EntityConverter> converters;
     private PersistenceManagerV2 persistence;
-    
-    
-    
+
+
+
     public EntityConverterManagerImpl(PersistenceManagerV2 persistence) {
         this.persistence = persistence;
         this.converters = new LinkedList<EntityConverter>();
     }
-    
-    
-    
+
+
+
     public void convertAll() throws DatabaseException {
-        // CONSIDER: One transaction for each converter?
-        this.persistence.writeAtomic(new Atomic() {
-            @Override
-            public void perform(Write write) {
-                for (EntityConverter ec : converters) {
+        for (final EntityConverter ec : this.converters) {
+            this.persistence.writeAtomicParallel(new Atomic() {
+                @Override
+                public void perform(Write write) {
                     try {
                         logger.info("Running Entity Converter: " + ec);
                         runConverter(ec, write);
@@ -44,25 +41,25 @@ public class EntityConverterManagerImpl {
                         logger.error("Error while converting entities", e);
                     }
                 }
-            }
-        });
+            });
+        }
     }
-    
-    
-    
+
+
+
     private void runConverter(EntityConverter ec, Write write) {
         logger.trace("Retrieving list of old entities...");
         List<Object> olds = ec.getOldEntities(write.read());
         List<Object> converted = new ArrayList<Object>(olds.size());
-        
+
         logger.trace("Converting " + olds.size() + " entities...");
         for (Object old : olds) {
             converted.add(ec.convertEntity(old));
         }
-        
+
         logger.trace("Persisting list of converted entities...");
         write.all(converted);
-        
+
         logger.trace("Deleting old entities (optional operation, may have no effect)...");
         ec.deleteOldEntities(olds, write);
     }
