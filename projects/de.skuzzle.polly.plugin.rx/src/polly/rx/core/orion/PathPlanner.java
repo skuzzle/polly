4eb6fc0cdd873a2da3101b82eec381cf0423118d
Simon Taddiken <simon.taddiken@gmail.com>
2014-01-03 20:03:45 +0100

    Adjusted edge weight for entry portals

--- projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/PathPlanner.java
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/PathPlanner.java
@@ -124,6 +124,7 @@ public class PathPlanner {
         
         private final double COST_DIAGONAL = 1.5 / 60;
         private final double COST_NORMAL = 1.0 / 60;
+        private final double COST_ENTRYPORTAL = COST_DIAGONAL * 3.0;
         private final double WORMHOLE_OFFSET = 100000.0;
         
         private final Set<Sector> done;
@@ -139,7 +140,7 @@ public class PathPlanner {
             switch (data.getType()) {
             case NORMAL:   return COST_NORMAL;
             case DIAGONAL: return COST_DIAGONAL;
-            case ENTRYPORTAL: return COST_NORMAL;
+            case ENTRYPORTAL: return COST_ENTRYPORTAL;
             case WORMHOLE:
                 final Wormhole hole = data.getWormhole();
                 double modifier = 1.0;
