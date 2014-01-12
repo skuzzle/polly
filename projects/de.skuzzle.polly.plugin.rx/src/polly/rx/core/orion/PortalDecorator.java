2f20c510602a66f41c14d14bd6c9ed6914e422ca
Simon Taddiken <simon.taddiken@gmail.com>
2014-01-12 13:18:08 +0100

    missing files

---
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/PortalDecorator.java
@@ -0,0 +1,91 @@
+package polly.rx.core.orion;
+
+import java.util.Date;
+
+import de.skuzzle.polly.tools.Check;
+import de.skuzzle.polly.tools.Equatable;
+import polly.rx.core.orion.model.Portal;
+import polly.rx.core.orion.model.PortalType;
+import polly.rx.core.orion.model.Sector;
+
+public class PortalDecorator implements Portal {
+
+    private final Portal wrapped;
+
+
+
+    public PortalDecorator(Portal wrapped) {
+        Check.notNull(wrapped);
+        this.wrapped = wrapped;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return this.wrapped.toString();
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return this.wrapped.hashCode();
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.wrapped.equals(obj);
+    }
+
+
+
+    @Override
+    public Class<?> getEquivalenceClass() {
+        return this.wrapped.getEquivalenceClass();
+    }
+
+
+
+    @Override
+    public boolean actualEquals(Equatable o) {
+        return this.wrapped.actualEquals(o);
+    }
+
+
+
+    @Override
+    public Sector getSector() {
+        return this.wrapped.getSector();
+    }
+
+
+
+    @Override
+    public String getOwner() {
+        return this.wrapped.getOwner();
+    }
+
+
+
+    @Override
+    public String getOwnerClan() {
+        return this.wrapped.getOwnerClan();
+    }
+
+
+
+    @Override
+    public PortalType getType() {
+        return this.wrapped.getType();
+    }
+
+
+
+    @Override
+    public Date getDate() {
+        return this.wrapped.getDate();
+    }
+}
---
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/PortalProviderDecorator.java
@@ -0,0 +1,54 @@
+package polly.rx.core.orion;
+
+import java.util.Collection;
+
+import polly.rx.core.orion.model.Portal;
+import polly.rx.core.orion.model.PortalType;
+import polly.rx.core.orion.model.Quadrant;
+import polly.rx.core.orion.model.Sector;
+
+public class PortalProviderDecorator implements PortalProvider {
+
+    private final PortalProvider wrapped;
+
+
+
+    public PortalProviderDecorator(PortalProvider wrapped) {
+        this.wrapped = wrapped;
+    }
+
+
+
+    @Override
+    public Collection<? extends Portal> getPortals(Sector sector) {
+        return this.wrapped.getPortals(sector);
+    }
+
+
+
+    public Collection<? extends Portal> getPortals(Sector sector, PortalType type) {
+        return this.wrapped.getPortals(sector, type);
+    }
+
+
+
+    @Override
+    public Portal getPersonalPortal(String ownerName) {
+        return this.wrapped.getPersonalPortal(ownerName);
+    }
+
+
+
+    @Override
+    public Portal getClanPortal(String nameOrTag) {
+        return this.wrapped.getClanPortal(nameOrTag);
+    }
+
+
+
+    @Override
+    public Collection<? extends Portal> getPortals(Quadrant quadrant, PortalType type) {
+        return this.wrapped.getPortals(quadrant, type);
+    }
+
+}
