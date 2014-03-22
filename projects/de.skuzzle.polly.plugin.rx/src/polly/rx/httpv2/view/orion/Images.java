a32f4c155d37701c1257ec63f244add3b37d8b37
Simon Taddiken <simon.taddiken@gmail.com>
2014-03-22 11:45:30 +0100

    cach sector images

--- projects/de.skuzzle.polly.plugin.rx/src/polly/rx/httpv2/view/orion/Images.java
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/httpv2/view/orion/Images.java
@@ -3,6 +3,8 @@ package polly.rx.httpv2.view.orion;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -12,8 +14,10 @@ import polly.rx.core.orion.model.SectorType;
 public final class Images {
     
     public static BufferedImage[] EMPTY_ROOM; 
-            
+
+    private final static Map<String, BufferedImage> CACHE;
     static {
+        CACHE = new HashMap<String, BufferedImage>();
         final SectorType e = SectorType.EMPTY;
         final int c = e.getMaxId() - e.getId() + 1;
         EMPTY_ROOM = new BufferedImage[c];
@@ -25,10 +29,19 @@ public final class Images {
     
 
     public static BufferedImage getImage(String name) {
+        synchronized (CACHE) {
+            final BufferedImage cached = CACHE.get(name);
+            if (cached != null) {
+                return cached;
+            }
+        }
         final InputStream is = Images.class.getResourceAsStream(name);
         if (is != null) {
             try {
                 final BufferedImage img = ImageIO.read(is);
+                synchronized (CACHE) {
+                    CACHE.put(name, img);
+                }
                 return img;
             } catch (IOException e) {
                 e.printStackTrace();
