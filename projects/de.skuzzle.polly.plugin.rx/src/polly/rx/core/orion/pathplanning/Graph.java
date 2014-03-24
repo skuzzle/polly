9dfc5cb1f6971543e77a6a8633238083e9c5f53c
Simon Taddiken <simon.taddiken@gmail.com>
2014-03-24 12:42:13 +0100

    removed import

--- projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/pathplanning/Graph.java
+++ projects/de.skuzzle.polly.plugin.rx/src/polly/rx/core/orion/pathplanning/Graph.java
@@ -10,7 +10,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.SortedSet;
